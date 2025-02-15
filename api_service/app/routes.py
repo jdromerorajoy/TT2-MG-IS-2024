a7ef31c341ae7b071ec2486c66a7cd0ce465a1ac
Jorge Romero <jdromerorajoy@MacBook-Pro-de-Jorge.local>
2025-02-15 18:05:33 -0300

    Improve logs

--- api_service/app/routes.py
+++ api_service/app/routes.py
@@ -1,13 +1,15 @@
 from flask import Blueprint, request, jsonify
-from app.middleware import require_auth, limiter, get_rate_limit
+from app.middleware import start_request, require_auth, limiter, get_rate_limit, end_request
 from app.services import PredictionClient
 from app.utils.logger_client import LoggerClient
 
 bp = Blueprint("api", __name__)
 
 @bp.route("/predict", methods=["POST"])
-@require_auth
-@limiter.limit(get_rate_limit)
+@start_request  # 📌 1️⃣ Inicia el timer y loggea el inicio de la request
+@require_auth   # 📌 2️⃣ Valida autenticación y loggea después de éxito
+@limiter.limit(get_rate_limit)  # 📌 3️⃣ Aplica Rate Limiting
+@end_request    # 📌 4️⃣ Registra el tiempo total de ejecución
 def predict():
     """Endpoint para obtener predicción desde `prediction_service`"""
 
@@ -17,7 +19,7 @@ def predict():
         LoggerClient.error("⚠️Solicitud sin datos de entrada")
         return jsonify({"error": "No input provided"}), 400
 
-    LoggerClient.info("📡 Enviando datos al modelo...")
+    LoggerClient.info(f"📡 Enviando datos al modelo: {inputs}")
     result = PredictionClient.get_prediction(inputs)
     LoggerClient.info(f"🎯 Predicción obtenida: {result}")
 
